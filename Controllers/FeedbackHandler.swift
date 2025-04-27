b0c05680f64bcbe6e10e73116622e50e094c7eab
Leonm1990 <leonmoore@hotmail.co.uk>
2025-04-27 22:15:21 +0100

    Create FeedbackHandler.swift

---
+++ Controllers/FeedbackHandler.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class FeedbackHandler {
+    static func sendFeedback(message: String) {
+        print("Feedback received: \(message)")
+        // Future: Send to server
+    }
+}
