672fa93e71c6943b4fc5ac3cb29d8414415e35e2
Leonm1990 <leonmoore@hotmail.co.uk>
2025-04-27 22:13:53 +0100

    Create BarcodeScannerView.swift

---
+++ Controllers/BarcodeScannerView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import AVFoundation
+
+struct BarcodeScannerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let controller = UIViewController()
+        // Future: Build barcode scanning
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
